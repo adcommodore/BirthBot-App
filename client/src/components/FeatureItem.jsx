f811564d8b6b925253aa24925eb1753a84c4602e
adcommodore <alexisdcommodore@gmail.com>
2022-09-28 10:15:29 -0400

    prepared files for deployment

--- client/src/components/FeatureItem.jsx
+++ client/src/components/FeatureItem.jsx
@@ -2,7 +2,7 @@ import { Card } from 'react-bootstrap';
 
 function FeatureItem({ text, src }) {
     return (
-        <Card className="card w-100 text-left px-3 my-3 pt-3">
+        <Card className="card w-100 text-left px-3 my-3 pt-3" style={{height: '30rem'}}>
             <Card.Img src={src} style={{opacity: '0.4'}}/>
             <Card.Body>
                 <Card.Title>{text}</Card.Title>
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 const corsOptions = require('./config/corsOptions');
@@ -8,15 +9,15 @@ const cookieParser = require('cookie-parser');
 const credentials = require('./middleware/credentials');
 const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
-const PORT = process.env.PORT;
-
+const PORT = process.env.PORT || 5002;
 
 app.use(logger);
 app.use(helmet());
+app.use(morgan('tiny'));
 app.use(credentials);
 app.use(cors(corsOptions));
-app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(errorHandler);
 
@@ -27,6 +28,10 @@ app.use('/admin', require('./routes/admin.routes'));
 app.use('/content', require('./routes/content.routes'));
 app.use('/user',  require('./routes/user.routes'));
 app.use('/sms', require('./routes/message.routes'));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+}
 
 const server = require('http').createServer(app);
 
